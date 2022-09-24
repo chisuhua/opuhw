1649e1624211e1b19f4282f39e2e5b9aebc370bc
Chi Suhua <chisuhua@gmail.com>
2022-09-24 18:01:59 +0800

    add regbank

--- hwlib/regbank.h
+++ hwlib/regbank.h
@@ -6,57 +6,68 @@
 #include "htl/enc.h"
 #include "hwlib/arbiter.h"
 
+using namespace ch::logic;
+
 namespace hwlib {
 
-template <unsigned BankSize, // cache size (Bytes)
-          unsigned RFBits>
+template <unsigned BankNums, // number of banks
+          unsigned RFBits,
           unsigned RFSize>
-struct Config {
+struct RFConfig {
   static constexpr unsigned num_banks  = BankNums;
   static constexpr unsigned bank_bits  = RFBits * num_banks;
   static constexpr unsigned rf_data_bits  = RFBits;
-  static constexpr unsigned rf_index_bits  = log2cail(RFSize);
+  static constexpr unsigned rf_index_bits  = log2ceil(RFSize);
   static constexpr unsigned rf_size  = RFSize;
 
   __interface (req_io, (
-    __in (ch_bit<rf_index_bits)> addr,
+    __in (ch_bit<rf_index_bits>) addr,
+    __in (ch_bool) valid,
     __in (ch_bool) read,
     __in (ch_bool) write,
     __in (ch_bit<rf_data_bits>) wdata,
     __out (ch_bit<rf_data_bits>) rdata
-    __out (ch_bool)           readdatavalid,
-    __out (ch_bool)           waitrequest
   ));
-
-}
+};
 
 template <typename Cfg>
 struct RegFile {
   __io (
-    ch_enq_io<(typename Cfg::req_io)> req
+    (typename Cfg::req_io) req
   );
 
 private:
+  using data_t = ch_bit<Cfg::rf_data_bits>;
 
 public:
 
   void describe() {
-    ch_mem<rf_data_bits, Cfg::rf_size> rf(0); // zero initialized
-    io.rdata = ch_sel(read, rf.read(addr), 0);
-    rf.write(addr, io.wdata, write);
+    ch_mem<data_t, Cfg::rf_size> rf(0); // zero initialized
+    ch_bool rd_en = io.req.valid && io.req.read;
+    ch_bool wr_en = io.req.valid && io.req.write;
+    io.req.rdata = ch_sel(rd_en, rf.read(io.req.addr), 0);
+    rf.write(io.req.addr, io.req.wdata, wr_en);
   }
 };
 
 template <typename Cfg>
-struct RegBanks {
+class RegBank {
+public:
+  using req_io_t = typename Cfg::req_io;
+
   __io (
-       (ch_vec<ch_enq_io(typename Cfg::req_io)>>) req
+      (ch_vec<req_io_t, Cfg::num_banks>) req
   );
 
   void describe() {
-    ch_vec<ch_module<RegFile<Cfg>>, Cfg::num_banks> regbanks;
+    ch_vec<ch_module<RegFile<Cfg>>, Cfg::num_banks> regbank;
     for (unsigned i = 0; i < Cfg::num_banks; ++i) {
-      regbanks.io.req[i] = io.req[i];
+      regbank[i].io.req.addr = io.req[i].addr;
+      regbank[i].io.req.valid = io.req[i].valid;
+      regbank[i].io.req.read = io.req[i].read;
+      regbank[i].io.req.write = io.req[i].write;
+      regbank[i].io.req.wdata = io.req[i].wdata;
+      io.req[i].rdata = regbank[i].io.req.rdata;
     }
   }
 };
--- tests/regbank.cpp
+++ tests/regbank.cpp
@@ -1,19 +1,69 @@
 #include "common.h"
 #include <hwlib/regbank.h>
 
+using namespace ch::htl;
 using namespace hwlib;
 
 TEST_CASE("regbank", "[regbank]") {
   SECTION("basic", "[basic]") {
     TESTX([]()->bool {
-      using Cfg = Config<512, 8, 4, 20, 32, 8>;
-      ch_device<regbank<Cfg>> regbank;
+      using Cfg = RFConfig<4, 32, 128>;
+      ch_device<RegBank<Cfg>> device;
       RetCheck ret;
 
-      ch_tracer tracer(regbank);
-      for (unsigned i = 0; i < 4, ++i) {
-        regbank.io.addr = 0;
+      ch_tracer sim(device);
+      ch_toVerilog("regbank.v", device);
+      sim.reset();
+      for (unsigned i = 0; i < 4; ++i) {
+        device.io.req[i].addr = i;
+        device.io.req[i].read = 0x0;
+        device.io.req[i].write = 0x1;
+        device.io.req[i].valid = true;
+        device.io.req[i].wdata = i*5;
       }
+      sim.step(2);
+      // read addr step 1
+      for (unsigned i = 0; i < 4; ++i) {
+        device.io.req[i].addr = i;
+        device.io.req[i].read = 0x1;
+        device.io.req[i].write = 0x0;
+        device.io.req[i].valid = true;
+      }
+      sim.step(2);
+      // read addr step 2
+      for (unsigned i = 0; i < 4; ++i) {
+        device.io.req[i].addr = i*4;
+        device.io.req[i].read = 0x1;
+        device.io.req[i].write = 0x0;
+        device.io.req[i].valid = true;
+      }
+      sim.step(2);
+      // write addr step 2
+      for (unsigned i = 0; i < 4; ++i) {
+        device.io.req[i].addr = i*4;
+        device.io.req[i].read = 0x0;
+        device.io.req[i].write = 0x1;
+        device.io.req[i].valid = true;
+        device.io.req[i].wdata = i*0xc;
+      }
+      sim.step(2);
+      // read addr step 2
+      for (unsigned i = 0; i < 4; ++i) {
+        device.io.req[i].addr = i*4;
+        device.io.req[i].read = 0x1;
+        device.io.req[i].write = 0x0;
+        device.io.req[i].valid = true;
+      }
+      sim.step(2);
+      // read addr step 1
+      for (unsigned i = 0; i < 4; ++i) {
+        device.io.req[i].addr = i;
+        device.io.req[i].read = 0x1;
+        device.io.req[i].write = 0x0;
+        device.io.req[i].valid = true;
+      }
+      sim.step(2);
+      sim.toVCD("regbank.vcd");
       return ret;
     });
   }
